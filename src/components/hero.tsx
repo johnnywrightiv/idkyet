bfd8c5df4b53f8b8e4fce7ee961b7c9c8057335f
Johnny Wright <johnnywrightiv@gmail.com>
2024-08-08 00:36:59 -0500

    prettier config

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,48 +3,45 @@ import NavigateButton from './navigate-button';
 import { lusitana } from '@/app/fonts';
 
 const Hero = (): JSX.Element => {
-  return (
-    <div className="flex min-h-screen flex-col items-center">
-      <div>
-        <Image
-          src="/hero-desktop.jpg"
-          alt="Desktop Hero"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: '100%', height: 'auto' }}
-          className="hidden md:block"
-          priority
-        />
-      </div>
-      <div>
-        <Image
-          src="/hero-mobile.jpg"
-          alt="Mobile Hero"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: '100%', height: 'auto' }}
-          className="md:hidden"
-          priority
-        />
-      </div>
-      <div className="flex flex-col mt-2 items-center">
-        <h1 className="text-2xl">Home Page</h1>
-        <h1 className={`${lusitana.className} text-lg`}>
-          Welcome to Our Website
-        </h1>
-        <p className={`${lusitana.className} text-lg`}>
-          Your success starts here.
-        </p>
-        <button className="bg-red-500" id="123">
-          abc
-        </button>
-      </div>
-      <NavigateButton route="/dogs" label="Test 404" />
-      <NavigateButton route="/dashboard" label="Dashboard" />
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen flex-col items-center">
+			<div>
+				<Image
+					src="/hero-desktop.jpg"
+					alt="Desktop Hero"
+					width={0}
+					height={0}
+					sizes="100vw"
+					style={{ width: '100%', height: 'auto' }}
+					className="hidden md:block"
+					priority
+				/>
+			</div>
+			<div>
+				<Image
+					src="/hero-mobile.jpg"
+					alt="Mobile Hero"
+					width={0}
+					height={0}
+					sizes="100vw"
+					style={{ width: '100%', height: 'auto' }}
+					className="md:hidden"
+					priority
+				/>
+			</div>
+			<div className="flex flex-col mt-2 items-center">
+				<h1 className="text-2xl">Home Page</h1>
+				<h1 className={`${lusitana.className} text-lg`}>
+					Welcome to Our Website
+				</h1>
+				<p className={`${lusitana.className} text-lg`}>
+					Your success starts here.
+				</p>
+			</div>
+			<NavigateButton route="/dogs" label="Test 404" />
+			<NavigateButton route="/dashboard" label="Dashboard" />
+		</div>
+	);
 };
 
 export default Hero;
--- src/components/navigate-button.tsx
+++ src/components/navigate-button.tsx
@@ -1,19 +1,18 @@
 import Link from 'next/link';
 
 interface NavigateButtonProps {
-  route: string;
-  label: string;
+	route: string;
+	label: string;
 }
 
 function NavigateButton({ route, label }: NavigateButtonProps) {
-  return (
-    <Link
-      href={route}
-      className="inline-block px-4 py-2 mt-2 bg-blue-500 hover:bg-blue-700 hover:scale-105 text-white rounded"
-    >
-      {label}
-    </Link>
-  );
+	return (
+		<Link
+			href={route}
+			className="inline-block px-4 py-2 mt-2 bg-blue-500 hover:bg-blue-700 hover:scale-105 text-white rounded">
+			{label}
+		</Link>
+	);
 }
 
 export default NavigateButton;
